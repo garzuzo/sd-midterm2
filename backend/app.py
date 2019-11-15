17f545aecec1dcd5a1ab4da7b6c2d8aa5702fdd5
garzuzo <garzuxo@gmail.com>
2019-11-15 00:50:59 -0500

    add endpoint tests. Fix dependencies. Add delete http method

--- backend/app.py
+++ backend/app.py
@@ -26,26 +26,48 @@ def read_user():
   return  res
 
 
-@app.route(api_url+'/users', methods=['POST'])
+@app.route(api_url+'/users/<string:id>', methods=['DELETE'])
 @cross_origin(origin='*')
-def create_user():
-  print()
-  idi=request.form['id']
-  name=request.form['name']
-        
-  if idi and name and request.method == "POST" :
-    idi = db.Users.insert({'id': idi, 'name':name})
-    resp = jsonify('User added successfully!')
-    resp.status_code = 200
-
+def delete_user(id):
+  answ=db.Users.delete_one({'id':id}).deleted_count
+  if answ==0 :
+    resp = jsonify('User does not exist')
+    resp.status_code = 404
+    return resp
+  else :
+    resp = jsonify('User removed successfully!')
+    resp.status_code = 204
     return resp
 
 
+
+
+@app.route(api_url+'/users', methods=['POST'])
+@cross_origin(origin='*')
+def create_user():
+
+
+  id=request.form['id']
+  name=request.form['name']
+        
+  if id and name and request.method == "POST" :
+    users=db.Users.find_one({'id':id})
+    if users is not None:
+      resp = jsonify('User already exists')
+      resp.status_code = 402
+      return resp
+    else:
+      db.Users.insert_one({'id': id, 'name':name})
+      resp = jsonify('User added successfully!')
+      resp.status_code = 201
+      return resp
   else :
-    return not_found()
+    resp = jsonify('Bad Request')
+    resp.status_code = 400
+    return resp
 
 if __name__ == "__main__":
   app.add_api('my_api.yaml', resolver=connexion.RestyResolver('api'))
 
-  app.run(port=3000)
+  app.run(port=3000, debug=True)
     
--- tests/test_app.py
+++ tests/test_app.py
@@ -3,8 +3,12 @@ import connexion
 import os
 from flask import Flask, request, flash, render_template, jsonify,json
 from flask_pymongo import pymongo
-import imp
-app=imp.load_source('app','backend/app.py')
+from bson.json_util import dumps
+import importlib.machinery
+
+loader = importlib.machinery.SourceFileLoader('app','backend/app.py')
+app = loader.load_module()
+
 
 flask_app = connexion.FlaskApp(__name__, specification_dir='../openapi/')
 flask_app.add_api('my_api.yaml', resolver=connexion.RestyResolver('api'))
@@ -18,16 +22,57 @@ def client():
 
 
 
-def test_read_clients(client):
+
+
+def test_valid_read_clients(client):
         
         response = client.get('/users')
         assert response.status_code == 200
     
-def test_cread_clients(client):
+
+
+
+def test_invalid_endpoint_read_clients(client):
         
-        response = client.get('/users')
-        assert response.status_code == 200
+        response = client.get('/users/32')
+        assert response.status_code == 405
     
+
+
+
+def test_valid_create_clients(client):
+       
+        header = {"Content-type": "application/x-www-form-urlencoded",
+          "Accept": "application/json"} 
+        response = client.post('/users', data=dict(id='abc12345', name='Marshmillow'), headers=header)
+        
+        assert response.status_code == 201
+        response_remove=client.delete('/users/abc12345')
+        assert response_remove.status_code == 204
+
+
+
+def test_delete_clients_unavailable(client):
+        response_remove=client.delete('/users/juanmaid')
+        assert response_remove.status_code == 404
+
+
+def test_invalid_create_clients(client):
+       
+        header = {"Content-type": "application/x-www-form-urlencoded",
+          "Accept": "application/json"}
+
+        response = client.post('/users', data=dict(id='1', name='Marshmillo'), headers=header)
+        
+        assert response.status_code == 402
     
+def test_invalid_body_req_create_clients(client):
+       
+        header = {"Content-type": "application/x-www-form-urlencoded",
+          "Accept": "application/json"}
+
+        response = client.post('/users', data=dict(id='bcc21912', names='Marshmillo'), headers=header)
+        
+        assert response.status_code == 400
     
     
