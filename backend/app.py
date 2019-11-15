8a761a5e139b58bb91aeb0161d8f876906f8a2f1
garzuzo <garzuxo@gmail.com>
2019-11-15 15:01:57 -0500

    fix env variable

--- backend/app.py
+++ backend/app.py
@@ -5,9 +5,7 @@ from bson.json_util import dumps
 import os
 from flask_cors import CORS, cross_origin
 app = connexion.FlaskApp(__name__, specification_dir='../openapi/')
-#app=Flask(__name__)
-#app.config['DEBUG'] = True
-url_mongo=os.environ['MONGO_FLASK']
+url_mongo="mongodb+srv://"+os.environ.get('MONGO_FLASK')+"@distribuidos-7a22s.mongodb.net/test?retryWrites=true&w=majority"
 client = pymongo.MongoClient(url_mongo)
 db = client.ds_db
 
